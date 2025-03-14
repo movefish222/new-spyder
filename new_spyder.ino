44628eea271bfb62c796f763af27c24e4ff37e5c
Movefish <2705441813@qq.com>
2025-03-14 16:27:10 +0800

    默认1

--- new_spyder.ino
+++ new_spyder.ino
@@ -185,40 +185,42 @@ byte yuyin_yanan[] = {
       0x7E, 0x05, 0x41, 0x00, 0x10, 0x54, 0xEF 
   };
 byte yuyin_voiceSetting[] = {
-      0x7E, 0x04, 0x31, 0x1E, 0x2B, 0xEF
+      0x7E, 0x04, 0x31, 0x30, 0x05, 0xEF
   };
 /**
  * 初始化函数
  */
 const int IN1 = 9, IN2 = 10, STBY = 11, PWM = 8;
-bool status[16];
-void setup() 
-{
-    for(int i = 0; i < 16; i++){
-      *(status + i) = true;
-    }
-    pinMode( 13, OUTPUT );
+void setup() {
+    pinMode(13, OUTPUT);
     pinMode( LED, OUTPUT);  //设置管脚为输出
     pinMode( IN1, OUTPUT); 
     pinMode( IN2, OUTPUT); 
     pinMode( PWM, OUTPUT);
-    pinMode( STBY, OUTPUT);
-        
+    pinMode( STBY, OUTPUT); 
+    digitalWrite(LED, LOW);//关闭LED
+    
+    digitalWrite(STBY, HIGH);
+    digitalWrite(PWM, HIGH);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    
+    
     Serial.begin(9600); // 波特率9600
     //while (!Serial);    // 等待打开串口
     
    //初始化软串口通信；
-   softSerial1.begin(9600);     
+   softSerial1.begin(9600);
    //监听软串口通信
    softSerial1.listen();
    delay(30);
    //读卡器自动读数据模式
-  //  dump_byte_array1(auto_read_config, 15);
-  //  delay(30);
-  //  //读卡器秘钥设置，系统默认秘钥A：FFFFFFFFFFFF  秘钥B：FFFFFFFFFFFF  
-  //  dump_byte_array1(key_Setting, 23);
-  //  dump_byte_array(yuyin_voiceSetting, 6);
-
+   dump_byte_array1(auto_read_config, 15);
+   delay(30);
+   //读卡器秘钥设置，系统默认秘钥A：FFFFFFFFFFFF  秘钥B：FFFFFFFFFFFF  
+   dump_byte_array1(key_Setting, 23);
+   //dump_byte_array(yuyin_voiceSetting, 6);
+  
   /*
    delay(100);
    //读卡器自动读数据模式
@@ -230,25 +232,15 @@ void setup()
    while (softSerial1.available())
    {
     softSerial1.read();
-    }
-    digitalWrite(STBY, HIGH);
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    delay(3000);
-    digitalWrite(LED, LOW);//关闭LED
-    digitalWrite(PWM, HIGH);
-  // dump_byte_array(yuyin_ruijin, 7);
-  // delay(3000);
-  // dump_byte_array(yuyin_ruijin, 7);
+  }
+  delay(3000);
+  dump_byte_array(yuyin_ruijin, 7);
 }
- 
+
 /**
  * Main主循环函数
  */
- 
-void loop() 
-{  
-   bool isDetect = false;
+void loop() {    
    digitalWrite(13, HIGH);
    byte ReceiveData[18]; // 接收数据数组
    byte size = sizeof(ReceiveData);
@@ -258,7 +250,7 @@ void loop()
   //读取从设备A传入的数据，并在串口监视器中显示  
   if(softSerial1.available()>0)
   { 
-    isDetect = true;
+    digitalWrite(LED, HIGH);//打开LED  
     memset(ReceiveData, 0, sizeof(ReceiveData));
     byte IndexMark = 0;
     //Serial.println(IndexMark);
@@ -266,8 +258,10 @@ void loop()
     {
       delay(1);
       ReceiveData[IndexMark] = softSerial1.read();
+      Serial.print(ReceiveData[IndexMark]);
       IndexMark++;
     }
+    Serial.print('\n');    
   }
 
     //判断从标签里读到的信息是什么
@@ -281,15 +275,10 @@ void loop()
         }
     if(count==16)
       {
-        if(!status[0]){
-          return;
-        }
-        status[0] = false; 
+         
         dump_byte_array(yuyin_ruijin, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
                  
     //突破第三道封锁线
@@ -302,15 +291,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[1]){
-          return;
-        }
-        status[1] = false;
         dump_byte_array(yuyin_tupodisandaofangxian, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
         
     //血战湘江
@@ -323,15 +306,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[2]){
-          return;
-        }
-        status[2] = false;
         dump_byte_array(yuyin_xuezhanxiangjiang, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
         
     //强渡乌江
@@ -344,15 +321,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[3]){
-          return;
-        }
-        status[3] = false;
         dump_byte_array(yuyin_qiangduwujiang, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
         
     //占领遵义
@@ -365,13 +336,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[4]) return;
-        status[4] = false;
         dump_byte_array(yuyin_zhanlingzunyi, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
         
     //四渡赤水
@@ -384,13 +351,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[5]) return;
-        status[5] = false;
         dump_byte_array(yuyin_siduchishui, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
         
     //巧渡金沙江
@@ -403,13 +366,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[6]) return;
-        status[6] = false;
         dump_byte_array(yuyin_qiaodujinshajiang, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
         
     //强渡大渡河
@@ -422,13 +381,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[7]) return;
-        status[7] = false;
         dump_byte_array(yuyin_qiangdudaduhe, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
 
     //飞夺泸定桥
@@ -441,13 +396,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[8]) return;
-        status[8] = false;
         dump_byte_array(yuyin_feiduoludingqiao, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
 
     //爬雪山
@@ -460,13 +411,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[9]) return;
-        status[9] = false;
         dump_byte_array(yuyin_paxueshan, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
             
     //懋功会师
@@ -479,13 +426,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[10]) return;
-        status[10] = false;  
         dump_byte_array(yuyin_maogonghuishi, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
             
     //过草地
@@ -498,13 +441,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[11]) return;
-        status[11] = false;
         dump_byte_array(yuyin_guocaodi, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
 
     //激战腊子口
@@ -517,13 +456,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[12]) return;
-        status[12] = false;
         dump_byte_array(yuyin_jizhanlazikou, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
             
     //会宁大会师
@@ -536,13 +471,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[13]) return;
-        status[13] = false;
         dump_byte_array(yuyin_huiningdahuishi, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
             
     //吴起镇会议
@@ -555,13 +486,9 @@ void loop()
         } 
     if(count==16)
       {
-        if(!status[14]) return;
-        status[14] = false;
         dump_byte_array(yuyin_wuqizhenhuiyi, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
         }
             
     //延安
@@ -574,19 +501,16 @@ void loop()
         }
     if(count==16)
       {
-        if(!status[15]) return;
-        status[15] = false;
         dump_byte_array(yuyin_yanan, 7);
-        digitalWrite(LED, HIGH);//打开LED
-        delay(500);  //延时时间需要适当修改
-        digitalWrite(LED, LOW);
-        return;
-      }
-    if(isDetect){
-      digitalWrite(LED, HIGH);
-      delay(500);  //亮灯500毫秒，延时时间需要适当修改
-      digitalWrite(LED, LOW);//关闭LED 
-    }
+        //digitalWrite(LED, HIGH);//打开LED
+        //delay(500);  //延时时间需要适当修改
+        }
+                     
+    
+
+
+    delay(500);  //亮灯500毫秒，延时时间需要适当修改
+    digitalWrite(LED, LOW);//关闭LED 
     digitalWrite(13, LOW); 
 }
 
